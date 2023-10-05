fa6f1cd8870596ea8f1226a03d958d7eb96d9e7f
Anamika Rawat <anamikarawart@gmail.com>
2023-10-06 01:05:07 +0530

    add new comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
-
-// import components
+import React from "react";
+import Header from './components/Header';
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import NewItems from "./components/NewItems";
+import FeaturesSecond from "./components/FeaturesSecond";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <div className="w-full max-w-[1440px] mx-auto bg-white">
+      <Header/>
+      <Hero />
+      <Features />
+      <NewItems />
+      <FeaturesSecond />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import {products } from '../data';
 
 const Products = () => {
-  return <div>Products</div>;
+  const {title, subtitle} = products;
+  return (
+    <section className='section'>
+      <div className='container mx-auto'>
+        <div>
+          <h2>{title}</h2>
+          <p>{subtitle}</p>
+        </div>
+        
+      </div>
+    </section>
+  );
+
 };
 
 export default Products;
